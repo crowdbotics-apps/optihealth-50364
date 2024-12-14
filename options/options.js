441b1d9e26492cad2364220e6f2b488ea2fe085a
Crowdbotics <team@crowdbotics.com>
2024-12-14 05:10:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "optihealth-50364",
+  url: "https://optihealth-50364.botics.co",
+  api: "https://optihealth-50364.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
